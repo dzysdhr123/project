ff517171821936145776faf7b3ea05102e221a8d
dzysdhr123 <43248981+dzysdhr123@users.noreply.github.com>
2019-03-25 21:08:43 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,14 +1,17 @@
 import cv2
 import os
 
+face_id = 0
+path, dirs, files = next(os.walk("/home/pi/ble-test-peripheral/node_modules/bleno/examples/echo/dataset"))
+file_count = len(files)
+face_id = file_count//30+1
+print(face_id)
+
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
 cam.set(4, 480) # set video height
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# For each person, enter one numeric face id
-face_id = input('\n enter user id end press <return> ==>  ')
 
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 # Initialize individual sampling face count
@@ -27,7 +30,7 @@ while(True):
         count += 1
 
         # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("/home/pi/ble-test-peripheral/node_modules/bleno/examples/echo/dataset/" + 'User.' +str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
 
         cv2.imshow('image', img)
 
